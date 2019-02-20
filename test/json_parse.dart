a1c8b2d886840344d774ceb4546896f3b1190c40
Wilson <l7960261@gmail.com>
2019-02-20 18:01:08 +0800

    add json_annotation package and json_parse.dart for unit test

---
+++ test/json_parse.dart
@@ -0,0 +1,14 @@
+import  'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:todo_flutter/model/mock.dart';
+import 'package:todo_flutter/model/data.dart';
+
+void main() {
+  group('jsonparse test', () {
+    test('mockdata test', () {
+      Data data1 = Data.fromJson(json.decode(JsonString.mockdata));
+      expect(data1.url, 'http://www.getdropbox.com/u/2/screencast.html');
+    });
+  });
+}
